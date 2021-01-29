3ac610709bdec2233a731e1fa87371c003a6b7d7
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-01-29 14:10:25 -0500

    index.js tasks

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ Do the following:
 
    HINT: no function required
 */
-
+const votingAge = 18;
+if(votingAge>=18){
+  console.log(true);
+}
 
 
 /*
@@ -30,7 +33,12 @@ Do the following:
 
    HINT: no function required
 */
-
+let a = 2;
+let b = 4;
+while(a<b){
+  a++;
+  console.log(a);
+}
 
 
 
@@ -45,7 +53,9 @@ Do the following:
 
    HINT: look up the Number method
 */
-
+let stringType = "1999";
+stringType = Number(stringType);
+console.log(stringType);
 
 
 
@@ -58,8 +68,8 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-    /*add your code here*/
+function multiply(a,b){
+    return a * b;
   }
 
 
@@ -74,8 +84,8 @@ Do the following:
    3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-    /*add your code here*/
+function dogYears(humanYears){
+    return humanYears * 7;
 }
 
 
@@ -107,8 +117,26 @@ Use the hungryDog function and feeding requirements below to do the following:
   NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(/*add your code here*/){
-    /*add your code here*/
+function hungryDog(weight, age){
+    if(age >= 1){
+      if(weight <= 5){
+        return .05 * weight;
+      }else if(weight <= 10){
+        return .04 * weight;
+      }else if(weight <= 15){
+        return .03 * weight;
+      }else if(weight > 15){
+        return .02 * weight;
+      }
+    }else if(age < 1){
+      if(age < .333){
+        return .1 * weight;
+      }else if(age < .583){
+        return .05 * weight;
+      }else if(age < 1){
+        return .04 * weight;
+      }
+    }
   }
 
 
@@ -133,9 +161,26 @@ Use the game function below to do the following:
   
   HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
-
+var rando = Math.random()*3;
 function game(user, computer){
-    /*add your code here*/
+  let win = "you win!";
+  let lose = "you lose!";
+  let tie = "it's a tie";
+  if(user==computer){
+    return tie;
+  }else if(user=='rock' && computer=='paper'){
+    return lose;
+  }else if(user=='rock' && computer=='scissors'){
+    return win;
+  }else if(user=='paper' && computer=='scissors'){
+    return lose;
+  }else if(user=='paper' && computer=='rock'){
+    return win;
+  }else if(user=='scissors' && computer=='rock'){
+    return lose;
+  }else if(user=='scissors' && computer=='paper'){
+    return win;
+  }
 }
   
   
@@ -151,8 +196,8 @@ Using the miles function below do the following:
   3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-    /*add your code here*/
+function miles(km){
+    return km * 0.621371;
   }
 
 
@@ -165,8 +210,8 @@ Using the feet function below do the following:
   3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-    /*add your code here*/
+function feet(cm){
+    return cm / 30.48;
   }
  
 
@@ -181,8 +226,8 @@ Using the annoyingSong function below do the following:
       "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-        /*add your code here*/
+function annoyingSong(number){
+    return number + " bottles of soda on the wall, " + number + " bottles of soda, take one down pass it around " + (number - 1) + " bottles of soda on the wall";
   }
 
 
@@ -201,8 +246,18 @@ Using the grade function below do the following:
    below should return 'you got an F'
 */
   
-function grade(/*Your Code here */){
-  /*Your Code here */
+function grade(score){
+    if(score>=90){
+      return "you got an A";
+    }else if(score>=80){
+      return "you got a B";
+    }else if(score>=70){
+      return "you got a C";
+    }else if(score>=60){
+      return "you got a D";
+    }else{
+      return "you got an F";
+    }
   }
   
   
